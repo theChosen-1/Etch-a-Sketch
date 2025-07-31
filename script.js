4f33f68967f8ea6c4057a02ab783a8a7eed9d3e6
Inshaal <inshaal81@gmail.com>
2025-07-31 13:28:37 +0500

    fixed color bug

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function createGrid(number) {
     const squares = document.querySelectorAll(".square");
     squares.forEach(square => {
         square.addEventListener("mouseenter", () => {
-            square.style.backgroundColor = black;
+            square.style.backgroundColor = "black";
         });
     });
 }
