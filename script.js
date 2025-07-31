4dd0459770dc439b80d2d0859ce969d18d331d63
Inshaal <inshaal81@gmail.com>
2025-07-31 13:22:51 +0500

    Added functionality to resize grid dynamically, and linked that property with a button.

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
 const container = document.querySelector(".container");
-// Create grid divs and append them to DOM
-let count = 0;
-while (count < 256) {
-    count += 1
-    const square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
+
+// Create grid divs and append them to the DOM
+function createGrid(number) {
+    let count = 0;
+    let squareSize = 960 / number;
+    let totalSquares = number * number;
+
+    while (count < totalSquares) {
+        count += 1;
+        const square = document.createElement("div");
+        square.classList.add("square");
+        // Set width and height dynamically
+        square.style.width = squareSize + "px";
+        square.style.height = squareSize + "px";
+        container.appendChild(square);
+    }
+    // adds hover to each of the squares
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => {
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = ;
+        });
+    });
 }
 
-const squares = document.querySelectorAll(".square");
-squares.forEach(square => {
-    square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = "black";
-    });
-});
+// function linked wiht button
+function resizeMe() {
+    let size = prompt("How many squares per side?");
+    if (size === null){
+        return;
+    }
+    size = parseInt(size);
+
+    if (size > 100) {
+        alert("Number too big! It should be less than 100.");
+        return;
+    }
+    else if (size < 1) {
+        alert("Number is too small!, It should be greater than 1.")
+        return;
+    }
+    else {
+        container.innerHTML = "";
+        createGrid(size)
+    }
+}
+
+// initializing button properties
+const resizeGrid = document.querySelector(".resizeGrid");
+resizeGrid.addEventListener("click", resizeMe);
+// page initially loads with a 16x16 grid
+createGrid(16);
